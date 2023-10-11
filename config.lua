66ab72ba1cdc648817cdbffa081e9a5c777650ed
wongJG <76810856+wongJG@users.noreply.github.com>
2023-10-11 22:31:24 +0800

    增加Telegram推送和解决开机读不到SIM卡 (#4)
    * add telegram notification
    
    * manually set DNS
    
    * support self-host bark server
    
    * keep reading SIM card till ready
    
    * Air780e net LED on/off option
    
    * Revert "support self-host bark server"
    
    This reverts commit d57c7a21112ada7af80e97a0638c1110044cf36e.
    
    * typo

--- config.lua
+++ config.lua
@@ -6,6 +6,9 @@ config.log_level = log.LOG_INFO
 -- esp32c3 / esp32s3
 config.board_type = "esp32s3"
 
+-- 是否关闭Air780e网络指示灯
+config.disable_netled = true
+
 -- 是否禁止RNDIS
 -- 禁止RNDIS可以防止流量流失
 config.disable_rndis = true
@@ -38,6 +41,14 @@ config.notification_channel = {
         webhook_url = "",
         -- 机器人安全设定中的关键词
         keyword = ""
+    },
+    -- telegram 机器人
+    telegram = {
+        enabled = true,
+        -- Webhook地址
+        webhook_url = "",
+        -- chat_id, 通过 https://api.telegram.org/bot<token>/getUpdates 获取
+        chat_id = ""
     },
     -- PushPlus 推送加
     pushplus = {
--- main.lua
+++ main.lua
@@ -40,6 +40,10 @@ sys.taskInit(function()
     local ip_address = wlan.getIP()
     log.info(logging_tag, "无线网络连接成功，IP地址："..ip_address)
 
+    log.info(logging_tag, "配置DNS服务器")
+    socket.setDNS(nil, 1, "119.29.29.29")
+    socket.setDNS(nil, 2, "223.5.5.5")
+
     log.info(logging_tag, "等待时间同步")
     sys.waitUntil("NTP_UPDATE")
     log.info(logging_tag, "时间同步完成")
@@ -55,10 +59,13 @@ sys.taskInit(function ()
     log.info(logging_tag, "Air780E已连接")
 
     log.info(logging_tag, "正在检查有无SIM卡")
-    at_command_result = air780.send_at_command_and_wait("AT+CPIN?", constants.air780_message_topic_sim_detected)
-    if not at_command_result then
-        log.error(logging_tag, "未检测到SIM卡")
-        return
+    while true do
+        at_command_result = air780.send_at_command_and_wait("AT+CPIN?", constants.air780_message_topic_sim_detected)
+        if at_command_result then
+            break
+        else
+            log.error(logging_tag, "未检测到SIM卡， 正在重试")
+        end
     end
 
     log.info(logging_tag, "正在配置短信功能")
@@ -85,6 +92,11 @@ sys.taskInit(function ()
             log.info(logging_tag, "GPRS未附着，将在5秒后重新检查")
             sys.wait(5000)
         end
+    end
+
+    if config.disable_netled then
+        log.info(logging_tag, "正在关闭NET灯闪烁")
+        air780.send_at_command("AT+CNETLIGHT=0")
     end
 
     log.info(logging_tag, "初始化完成，等待新短信...")
